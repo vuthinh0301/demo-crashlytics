c2233a9e18e40fe48a95b1495495c03f778755eb
vuthinh0301 <vuthinh0301@gmail.com>
2023-07-17 09:05:10 +0700

    Initial Commit

---
+++ CrashlyticsDemo/CrashlyticsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashlyticsDemoApp.swift
+//  CrashlyticsDemo
+//
+//  Created by Vu Duc Thinh on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrashlyticsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
